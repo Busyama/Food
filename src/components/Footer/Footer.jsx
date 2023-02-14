8f93213df424e10d3f664fa2ba19db6066f54fce
Пользователь <busyama@yandex.ru>
2023-02-14 23:10:27 +0300

    second

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => {
+  return <footer className="footer">© «Интернет-магазин DogFood.ru»</footer>;
+};
+
+
+  
+                  
+
+export default Footer;
